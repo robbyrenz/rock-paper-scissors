a7f422260ab10d7bfc19339c693925edaa934986
Robby Renz <nxgrenz@gmail.com>
2019-02-19 12:00:29 -0600

    coded the computer's AI and shows its output to the screen

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 let clicked = false
+let computerChoice
+let options = ['rock', 'paper', 'scissors','rock', 'paper', 'scissors','rock', 'paper', 'scissors']
 
 window.onload = buttonFuntionality
 
 function wordCreator(foo) {
     if (!clicked) {
-        clicked = true
+        // clicked = true
         let place = document.getElementById("user-answer")
         // using innerHTML rather than appendText so that it rewrites the user's previous option
         place.innerHTML = `You chose ${foo}`
+        computer()
 
         // wait around a second before letting the user choose another icon
-        setTimeout(() => {
-            clicked = false
-        }, 1000)
+        // setTimeout(() => {
+        //     clicked = false
+        // }, 1000)
     }
 }
 
@@ -46,5 +49,8 @@ function wordInitialiser(btn) {
 }
 
 function computer() {
-
+    computerChoice = options[Math.floor(Math.random() * options.length)]
+    // console.log(computerChoice)
+    let computerAnswer = document.getElementById("computer-answer")
+    computerAnswer.innerHTML = `Computer chose ${computerChoice}`
 }
