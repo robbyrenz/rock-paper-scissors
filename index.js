a5faa96bb0bd4ca8be1220fd9e4a480fe084af41
Robby Renz <nxgrenz@gmail.com>
2019-02-19 12:31:18 -0600

    computer & final verdict fully working, webpage fully working

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 let clicked = false
 let computerChoice
 let options = ['rock', 'paper', 'scissors','rock', 'paper', 'scissors','rock', 'paper', 'scissors']
+let userChoice
 
 window.onload = buttonFuntionality
 
 function wordCreator(foo) {
+    userChoice = foo
+    // console.log(userChoice)
     if (!clicked) {
         // clicked = true
         let place = document.getElementById("user-answer")
         // using innerHTML rather than appendText so that it rewrites the user's previous option
         place.innerHTML = `You chose ${foo}`
         computer()
+        verdict()
 
         // wait around a second before letting the user choose another icon
         // setTimeout(() => {
@@ -53,4 +57,26 @@ function computer() {
     // console.log(computerChoice)
     let computerAnswer = document.getElementById("computer-answer")
     computerAnswer.innerHTML = `Computer chose ${computerChoice}`
+}
+
+function verdict() {
+    let verdict = document.getElementById("verdict")
+    // its a tie
+    if (userChoice === 'rock' && computerChoice === 'rock' || userChoice === 'paper' && computerChoice === 'paper' || userChoice === 'scissors' && computerChoice === 'scissors')
+        verdict.innerHTML = 'ITS A TIE!!!'
+    // for paper
+    else if (userChoice === 'paper' && computerChoice === 'rock')
+        verdict.innerHTML = 'YOU WON!'
+    else if (userChoice === 'paper' && computerChoice === 'scissors')
+        verdict.innerHTML = 'YOU LOST!'
+    // for rock
+    else if (userChoice === 'rock' && computerChoice === 'paper')
+        verdict.innerHTML = 'YOU LOST!'
+    else if (userChoice === 'rock' && computerChoice === 'scissors')
+        verdict.innerHTML = 'YOU WON!'
+    // for scissors
+    else if (userChoice === 'scissors' && computerChoice === 'rock')
+        verdict.innerHTML = 'YOU LOST!'
+    else if (userChoice === 'scissors' && computerChoice === 'paper')
+        verdict.innerHTML = 'YOU WON!'
 }
